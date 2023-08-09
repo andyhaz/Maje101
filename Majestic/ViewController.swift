5b5b5d88564b454dd31943c74658bca9a003e64b
andrew <andyhaz1970@gmail.com>
2023-08-09 12:13:26 -0500

    Initial Commit

---
+++ Majestic/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Majestic
+//
+//  Created by andyhaz on 8/9/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
